1ca15f1947f3dbb9b61ccde0582b2a9cf3462ab3
eliot390 <eliot390@github.com>
2022-11-29 10:39:36 -0800

    camera integration and design updates

--- app/src/main/java/com/example/scanify/Home.kt
+++ app/src/main/java/com/example/scanify/Home.kt
@@ -2,19 +2,38 @@ package com.example.scanify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.content.Intent
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        val btnHistory = findViewById<ImageButton>(R.id.btnHistory)
+        val btnScan = findViewById<ImageButton>(R.id.btnScan)
+        val btnLogout = findViewById<ImageButton>(R.id.btnLogout)
 
-        val actionBar = supportActionBar
+        btnHistory.setOnClickListener {
+            val intent = Intent(this, Scan::class.java)
+            startActivity(intent)
+        }
 
+        btnScan.setOnClickListener {
+            val intent = Intent(this, Scan::class.java)
+            startActivity(intent)
+        }
+
+        btnLogout.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        /*val actionBar = supportActionBar
         if(actionBar != null){
             actionBar.title = "Home"
         }
-
-        actionBar?.setDisplayHomeAsUpEnabled(true)
+        actionBar?.setDisplayHomeAsUpEnabled(true)*/
     }
 }
--- app/src/main/java/com/example/scanify/Scan.kt
+++ app/src/main/java/com/example/scanify/Scan.kt
@@ -1,11 +1,116 @@
 package com.example.scanify
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.budiyev.android.codescanner.*
+import kotlinx.android.synthetic.main.camera_page.*
 
-class Scan : AppCompatActivity() {
+private const val CAMERA_REQUEST_CODE = 101
+
+class Scan : AppCompatActivity(){
+
+    private lateinit var codeScanner: CodeScanner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_scan)
+        setContentView(R.layout.camera_page)
+
+        setupPermissions()
+        codeScanner()
+
+        /*val scannerView = findViewById<CodeScannerView>(R.id.scanner_view)
+
+        codeScanner = CodeScanner(this, scannerView)
+
+        // Parameters (default values)
+        codeScanner.camera = CodeScanner.CAMERA_BACK // or CAMERA_FRONT or specific camera id
+        codeScanner.formats = CodeScanner.ALL_FORMATS // list of type BarcodeFormat,
+        // ex. listOf(BarcodeFormat.QR_CODE)
+        codeScanner.autoFocusMode = AutoFocusMode.SAFE // or CONTINUOUS
+        codeScanner.scanMode = ScanMode.SINGLE // or CONTINUOUS or PREVIEW
+        codeScanner.isAutoFocusEnabled = true // Whether to enable auto focus or not
+        codeScanner.isFlashEnabled = false // Whether to enable flash or not
+
+        scannerView.setOnClickListener {
+            codeScanner.startPreview()
+        }*/
+
     }
+
+    private fun codeScanner(){
+        codeScanner = CodeScanner(this, scanner_view)
+
+        codeScanner.apply {
+            camera = CodeScanner.CAMERA_BACK
+            formats = CodeScanner.ALL_FORMATS
+
+            autoFocusMode = AutoFocusMode.SAFE
+            scanMode = ScanMode.CONTINUOUS
+            isAutoFocusEnabled = true
+            isFlashEnabled = false
+
+            decodeCallback = DecodeCallback {
+                runOnUiThread {
+
+                }
+            }
+
+            errorCallback = ErrorCallback {
+                runOnUiThread {
+                    Log.e("Main", "Camera initialization error: ${it.message}")
+                }
+            }
+        }
+
+        scanner_view.setOnClickListener{
+            codeScanner.startPreview()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
+    }
+
+    private fun setupPermissions() {
+        val permission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+        if (permission != PackageManager.PERMISSION_GRANTED){
+            makeRequest()
+        }
+    }
+
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.CAMERA),
+            CAMERA_REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {
+            CAMERA_REQUEST_CODE -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this, "You need camera permission", Toast.LENGTH_SHORT).show()
+                } else {
+                    //success
+                }
+            }
+        }
+    }
+
 }
